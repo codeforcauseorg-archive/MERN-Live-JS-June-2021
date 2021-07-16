4ac28915697923d128b10637c5338370780e8a74
KeenWarrior <anujgargcse@gmail.com>
2021-07-16 22:16:38 +0530

    l 9

--- lecture-08/server/index.js
+++ lecture-08/server/index.js
@@ -1,12 +1,31 @@
 let express = require("express");
 
-let app = express()
+let app = express();
+
+app.use(express.urlencoded());
+
+let users = new Map();
+
+users.set("anuj", "Anuj Garg");
+users.set("mohit", "Mohit Sharma");
+users.set("rav", "Ravi Saini");
+
 
 let handleroot = function(req, res){
-    res.send("Yayyy. We responded from server");
+    if(req.query.name && users.has(req.query.name)){
+        res.send("Hello " + users.get(req.query.name));
+    } else {
+        res.send("Hello who ever you are");
+    }
 }
 
-app.get("/home", handleroot);
+
+let handlebody = function(req, res){
+    console.log(req.body);
+    res.send("We got it");
+}
+
+app.post("/", handlebody);
 
 app.listen(3000, function(err){
     if(!err){
